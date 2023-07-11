adfac4fbcbc0d3956bc0094560c7d112fbb6632c
MASTER_ARPIT <brajeshkumarmes02@gmail.com>
2023-07-11 10:42:00 +0530

    add

---
+++ addition_of_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sum;
+
+    printf("enter the number=");
+    scanf("%d \n %d",&a,&b);
+
+    sum=a+b;
+
+    printf("sum=%d",sum);
+
+    return 0;
+}
